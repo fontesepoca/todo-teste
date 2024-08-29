e5aa665d1bae5110bccbea6a8d6f6b42edffa1ea
Gabriel Freitas <gabriel.freitas@in8.com.br>
2024-08-29 10:45:26 -0300

    feat: adding style to the browser

--- front-fakestore/src/pages/_app.tsx
+++ front-fakestore/src/pages/_app.tsx
@@ -2,11 +2,15 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import MyProvider from "fakestore/context/myProvider";
 import { StrictMode } from "react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <StrictMode>
+        <Head>
+          <link rel="icon" href="https://fakestoreapi.com/icons/logo.png" />
+        </Head>
         <MyProvider>
           <Component {...pageProps} />
         </MyProvider>
